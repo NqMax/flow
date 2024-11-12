02f988d8d360d31fab206411af6f91f5dfb344d6
Max <111196993+NqMax@users.noreply.github.com>
2024-11-11 21:35:48 -0600

    feat: add geist fonts to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "geist": "var(--font-geist-sans)",
+        "geist-mono": "var(--font-geist-mono)",
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
